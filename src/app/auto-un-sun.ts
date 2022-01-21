986253fe6df78d3f7f8ac905785d56cee7431774
balajiklr <balajikoliyanur@gmail.com>
2022-01-21 22:55:24 +0530

    1 file modified

--- src/app/auto-un-sun.ts
+++ src/app/auto-un-sun.ts
@@ -1,35 +1,35 @@
-export function AutoUnsubscribe() {
-  debugger;
-  return function (constructor) {
-    const orig = constructor.prototype.ngOnDestroy;
-    constructor.prototype.ngOnDestroy = function () {
-      for (const prop in this) {
-        const property = this[prop];
-        if (typeof property.subscribe === 'function') {
-          property.unsubscribe();
-        }
-      }
-      orig.apply();
-    };
-  };
-}
-
-// export function AutoUnsubscribe(blackList = []) {
-//   return function(constructor) {
-//     const original = constructor.prototype.ngOnDestroy;
-
-//     constructor.prototype.ngOnDestroy = function() {
-//       for (let prop in this) {
+// export function AutoUnsubscribe() {
+//   debugger;
+//   return function (constructor) {
+//     const orig = constructor.prototype.ngOnDestroy;
+//     constructor.prototype.ngOnDestroy = function () {
+//       for (const prop in this) {
 //         const property = this[prop];
-//         if (!blackList.includes(prop)) {
-//           if (property && typeof property.unsubscribe === 'function') {
-//             property.unsubscribe();
-//           }
+//         if (typeof property.subscribe === 'function') {
+//           property.unsubscribe();
 //         }
 //       }
-//       original &&
-//         typeof original === 'function' &&
-//         original.apply(this, arguments);
+//       orig.apply();
 //     };
 //   };
 // }
+
+export function AutoUnsubscribe(blackList = []) {
+  return function(constructor) {
+    const original = constructor.prototype.ngOnDestroy;
+
+    constructor.prototype.ngOnDestroy = function() {
+      for (let prop in this) {
+        const property = this[prop];
+        if (!blackList.includes(prop)) {
+          if (property && typeof property.unsubscribe === 'function') {
+            property.unsubscribe();
+          }
+        }
+      }
+      original &&
+        typeof original === 'function' &&
+        original.apply(this, arguments);
+    };
+  };
+}
